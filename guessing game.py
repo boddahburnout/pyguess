b3cc11cae9ed7c2dbc3a4df3fa1b251a32a59982
BuildTools <unconfigured@null.spigotmc.org>
2017-05-08 11:08:05 -0400

    Signed-off-by: Dakota Vega <Dvega9876@gmail.com

---
+++ guessing game.py
@@ -0,0 +1,28 @@
+from random import randint
+found = False
+tries = 0
+lowval = int(input("Lowest value: "))
+highval = int(input("Highest value: "))
+answer = randint(lowval, highval)
+while not found:
+    guess = int(input("Guess: "))
+    tries = tries + 1
+    if guess == answer:
+        print ("Correct")
+        print ("Tries: "+ str(tries))
+        again = input("Play Again?: ")
+        if again == "yes":
+            lowval = int(input("Lowest value: "))
+            highval = int(input("Highest value: "))
+            answer = randint(lowval, highval)
+            guess = 0
+            tries = 0
+            found = False
+        if again != "yes":
+                print ("See ya!")
+                found = True
+        if guess > highval:
+            print (str(guess)+" is greater then "+ str(highval)+" Duh")
+    else:
+        print ("Wrong")
+            
