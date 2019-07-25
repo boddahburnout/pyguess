ebd8843144ceb696af3d2508667131f2cbbcab08
boddahburnout <dvega9876@gmail.com>
2019-07-25 18:39:28 -0400

    rewrite
    old code was confusing and sloppy fixed this issue with rewrite

--- guessing game.py
+++ guessing game.py
@@ -1,28 +1,26 @@
 from random import randint
-found = False
-tries = 0
-lowval = int(input("Lowest value: "))
-highval = int(input("Highest value: "))
-answer = randint(lowval, highval)
-while not found:
-    print ("Guess a number "+ str(lowval) +"-"+ str(highval))
-    guess = int(input("Guess: "))
-    tries = tries + 1
-    if guess == answer:
-        print ("Correct")
-        print ("Tries: "+ str(tries))
-        again = input("Play Again?: ")
-        if again == "yes":
-            lowval = int(input("Lowest value: "))
-            highval = int(input("Highest value: "))
-            answer = randint(lowval, highval)
-            guess = 0
-            tries = 0
-            found = False
-        if again != "yes":
-                print ("See ya!")
-                found = True
-        if guess > highval:
-            print (str(guess)+" is greater then "+ str(highval)+" Duh")
+reset = True
+while reset:
+    low = int(input('Low number> '))
+    high = int(input('Highest number> '))
+    if high < low:
+        print('invalid number range {}-{}'.format(low,high))
     else:
-        print ("Wrong")
+        numb = randint(low,high)
+        reset = False
+        guessing = True
+        tries = 0
+    while guessing:
+        guess = int(input('Guess {}-{}> '.format(low,high)))
+        tries = tries + 1
+        if guess > numb:
+            print('The number is lower than {}'.format(guess))
+        if guess < numb:
+            print('The number is higher then {}'.format(guess))
+        if guess == numb:
+            print('Correct, It took you {} tries to guess a number between {} and {}'.format(tries,low,high))
+            if input('Play again? (y or n)> ') == 'y':
+                reset = True
+                guessing = False
+            else:
+                guessing = False
